d274d4ada87a95f9bb9472c9aa6d475c49d1a6f8
Josh Taylor <jtaylor@gma-us.com>
2015-06-10 11:43:48 -0400

    intial project version

---
+++ stat01.R
@@ -0,0 +1,6 @@
+#######################################################################
+#Program: stat01.R
+#Project: gitTest
+#Author: Josh Taylor, Greylock McKinnon Associates
+#Last Edited: 6/10/15
+#######################################################################
