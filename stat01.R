045eec21e05d4f952a0e29941f0d3017f732fbec
Josh Taylor <jtaylor@gma-us.com>
2015-06-11 10:22:03 -0400

    I added a regression to this file

--- stat01.R
+++ stat01.R
@@ -4,3 +4,9 @@
 #Author: Josh Taylor, Greylock McKinnon Associates
 #Last Edited: 6/10/15
 #######################################################################
+
+
+wage <- read.csv("G:/gitTest/Data/wage.csv")
+
+wageReg = lm(wage ~ educ + exper + tenure, data = wage)
+summary(wageReg)
