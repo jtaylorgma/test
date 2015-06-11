7cf828592ad93e2da32554d6764b6f8d1233feed
Josh Taylor <jtaylor@gma-us.com>
2015-06-11 10:45:22 -0400

    I added a regression to this file also

--- stat01.py
+++ stat01.py
@@ -2,6 +2,19 @@
 #Program: stat01.py
 #Project: gitTest
 #Author: Josh Taylor, Greylock Mckinnon Associates
-#Last Edited: 6/10/2015
+#Last Edited: 6/11/2015
 ########################################################################################################################
 
+
+import pandas as pd
+import statsmodels.api as sm
+
+#path to this file: "C:\Users\jtaylor\Projects\Git\Code\test\stat01.py"
+
+df = pd.read_csv("G:\gitTest\Data\wage.csv")
+
+y = df['wage']
+x = df[['educ', 'exper', 'tenure']]
+
+wageReg = sm.OLS(y,x).fit()
+print wageReg.summary()
